7141c34fc429f2f5fd904333d08e71fabb36c2f5
ivy <ivy@mega-ivysaur.local>
2025-07-10 00:49:05 +0800

    Done for today

--- famelineui/src/components/LuvaLocation.jsx
+++ famelineui/src/components/LuvaLocation.jsx
@@ -61,110 +61,116 @@ const LuvaLocation = () => {
 
       {/* Progress Bar */}
       <div className="progress-bar-container">
-        <div className="progress-bar">
-          <svg
-            width="575"
-            height="59"
-            viewBox="0 0 575 59"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M29.0186 2.5C43.6643 2.5 55.5371 14.3728 55.5371 29.0186C55.5371 43.6643 43.6644 55.5371 29.0186 55.5371C14.3728 55.5371 2.5 43.6643 2.5 29.0186C2.5 14.3728 14.3728 2.5 29.0186 2.5Z"
-              fill="#CCFFCD"
-              fillOpacity="0.2"
-              stroke="#CCFFD9"
-              strokeWidth="5"
-            ></path>
-            <g filter="url(#filter0_d_318_2817)">
-              <rect
-                x="15.5684"
-                y="15.5681"
-                width="26.9008"
-                height="24.9793"
-                fill="url(#pattern0_318_2817)"
-                shapeRendering="crispEdges"
-              ></rect>
-            </g>
-            <path
-              d="M133.019 2.5C147.664 2.5 159.537 14.3728 159.537 29.0186C159.537 43.6643 147.664 55.5371 133.019 55.5371C118.373 55.5371 106.5 43.6643 106.5 29.0186C106.5 14.3728 118.373 2.5 133.019 2.5Z"
-              stroke="white"
-              strokeWidth="5"
-            ></path>
-            <path
-              d="M236.059 2.5C250.704 2.5 262.577 14.3728 262.577 29.0186C262.577 43.6643 250.704 55.5371 236.059 55.5371C221.413 55.5371 209.54 43.6643 209.54 29.0186C209.54 14.3728 221.413 2.5 236.059 2.5Z"
-              fill="black"
-              stroke="white"
-              strokeWidth="5"
-            ></path>
-            <path
-              d="M545.898 2.5C560.544 2.5 572.417 14.3728 572.417 29.0186C572.417 43.6643 560.544 55.5371 545.898 55.5371C531.252 55.5371 519.379 43.6643 519.379 29.0186C519.379 14.3728 531.252 2.5 545.898 2.5Z"
-              fill="black"
-              stroke="white"
-              strokeWidth="5"
-            ></path>
-            <path
-              d="M442.618 2.5C457.264 2.5 469.137 14.3728 469.137 29.0186C469.137 43.6643 457.264 55.5371 442.618 55.5371C427.972 55.5371 416.1 43.6643 416.1 29.0186C416.1 14.3728 427.972 2.5 442.618 2.5Z"
-              fill="black"
-              stroke="white"
-              strokeWidth="5"
-            ></path>
-            <path
-              d="M339.338 2.5C353.984 2.5 365.857 14.3728 365.857 29.0186C365.857 43.6643 353.984 55.5371 339.338 55.5371C324.693 55.5371 312.82 43.6643 312.82 29.0186C312.82 14.3728 324.693 2.5 339.338 2.5Z"
-              fill="black"
-              stroke="white"
-              strokeWidth="5"
-            ></path>
-            <defs>
-              <filter
-                id="filter0_d_318_2817"
-                x="11.5684"
-                y="15.5681"
-                width="34.9009"
-                height="32.9793"
-                filterUnits="userSpaceOnUse"
-                colorInterpolationFilters="sRGB"
-              >
-                <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
-                <feColorMatrix
-                  in="SourceAlpha"
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                  result="hardAlpha"
-                ></feColorMatrix>
-                <feOffset dy="4"></feOffset>
-                <feGaussianBlur stdDeviation="2"></feGaussianBlur>
-                <feComposite in2="hardAlpha" operator="out"></feComposite>
-                <feColorMatrix
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                ></feColorMatrix>
-                <feBlend
-                  mode="normal"
-                  in2="BackgroundImageFix"
-                  result="effect1_dropShadow_318_2817"
-                ></feBlend>
-                <feBlend
-                  mode="normal"
-                  in="SourceGraphic"
-                  in2="effect1_dropShadow_318_2817"
-                  result="shape"
-                ></feBlend>
-              </filter>
-              <pattern
-                id="pattern0_318_2817"
-                patternContentUnits="objectBoundingBox"
-                width="1"
-                height="1"
-              >
-                <use
-                  xlinkHref="#image0_318_2817"
-                  transform="matrix(0.0013986 0 0 0.00149701 -1.06993 -0.684132)"
-                ></use>
-              </pattern>
-            </defs>
-          </svg>
+        <div className="progress-indicators">
+          <div className="progress-indicator active"></div>
+          <div className="progress-indicator"></div>
+          <div className="progress-indicator"></div>
+          <div className="progress-indicator"></div>
+          <div className="progress-indicator"></div>
         </div>
+        <svg
+          className="progress-bar-svg"
+          width="575"
+          height="59"
+          viewBox="0 0 575 59"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M29.0186 2.5C43.6643 2.5 55.5371 14.3728 55.5371 29.0186C55.5371 43.6643 43.6644 55.5371 29.0186 55.5371C14.3728 55.5371 2.5 43.6643 2.5 29.0186C2.5 14.3728 14.3728 2.5 29.0186 2.5Z"
+            fill="#CCFFCD"
+            fillOpacity="0.2"
+            stroke="#CCFFD9"
+            strokeWidth="5"
+          />
+          <g filter="url(#filter0_d_318_2817)">
+            <rect
+              x="15.5684"
+              y="15.5681"
+              width="26.9008"
+              height="24.9793"
+              fill="url(#pattern0_318_2817)"
+              shapeRendering="crispEdges"
+            />
+          </g>
+          <path
+            d="M133.019 2.5C147.664 2.5 159.537 14.3728 159.537 29.0186C159.537 43.6643 147.664 55.5371 133.019 55.5371C118.373 55.5371 106.5 43.6643 106.5 29.0186C106.5 14.3728 118.373 2.5 133.019 2.5Z"
+            stroke="white"
+            strokeWidth="5"
+          />
+          <path
+            d="M236.059 2.5C250.704 2.5 262.577 14.3728 262.577 29.0186C262.577 43.6643 250.704 55.5371 236.059 55.5371C221.413 55.5371 209.54 43.6643 209.54 29.0186C209.54 14.3728 221.413 2.5 236.059 2.5Z"
+            fill="black"
+            stroke="white"
+            strokeWidth="5"
+          />
+          <path
+            d="M545.898 2.5C560.544 2.5 572.417 14.3728 572.417 29.0186C572.417 43.6643 560.544 55.5371 545.898 55.5371C531.252 55.5371 519.379 43.6643 519.379 29.0186C519.379 14.3728 531.252 2.5 545.898 2.5Z"
+            fill="black"
+            stroke="white"
+            strokeWidth="5"
+          />
+          <path
+            d="M442.618 2.5C457.264 2.5 469.137 14.3728 469.137 29.0186C469.137 43.6643 457.264 55.5371 442.618 55.5371C427.972 55.5371 416.1 43.6643 416.1 29.0186C416.1 14.3728 427.972 2.5 442.618 2.5Z"
+            fill="black"
+            stroke="white"
+            strokeWidth="5"
+          />
+          <path
+            d="M339.338 2.5C353.984 2.5 365.857 14.3728 365.857 29.0186C365.857 43.6643 353.984 55.5371 339.338 55.5371C324.693 55.5371 312.82 43.6643 312.82 29.0186C312.82 14.3728 324.693 2.5 339.338 2.5Z"
+            fill="black"
+            stroke="white"
+            strokeWidth="5"
+          />
+          <defs>
+            <filter
+              id="filter0_d_318_2817"
+              x="11.5684"
+              y="15.5681"
+              width="34.9009"
+              height="32.9793"
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy="4" />
+              <feGaussianBlur stdDeviation="2" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_318_2817"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_318_2817"
+                result="shape"
+              />
+            </filter>
+            <pattern
+              id="pattern0_318_2817"
+              patternContentUnits="objectBoundingBox"
+              width="1"
+              height="1"
+            >
+              <use
+                xlinkHref="#image0_318_2817"
+                transform="matrix(0.0013986 0 0 0.00149701 -1.06993 -0.684132)"
+              />
+            </pattern>
+          </defs>
+        </svg>
       </div>
 
       {/* Top Right Icon - Back to HomePage */}
@@ -231,7 +237,7 @@ const LuvaLocation = () => {
                 {environmentOptions.map((option, index) => (
                   <div
                     key={index}
-                    className="dropdown-option"
+                    className={`dropdown-option ${formData.environment === option ? "active" : ""}`}
                     onClick={() => handleEnvironmentSelect(option)}
                   >
                     {option}
